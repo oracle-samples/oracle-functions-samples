72ed54a12129bd34cd867cf2af4e53956f18c5a0
Deepak Devadathan <deepak.devadathan@oracle.com>
2022-10-27 03:07:59 +0000

    dotnet function for query adb using oracle client
    Signed-off-by: Deepak Devadathan <deepak.devadathan@oracle.com>

---
+++ samples/oci-adb-client-runsql-dotnet/Models/OutputMessage.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+// using System.Collections;
+// using System.Collections.Specialized;
+
+namespace RunSQL
+{
+
+    class OutputDetails
+    {
+        public string sql { get; set; }
+
+        public List<Dictionary<string, string>> result { get; set; }
+
+    }
+
+
+}
